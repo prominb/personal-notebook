652e051dd4a920b79cf935c03f261e88cbf4b014
Roman_M <prominb@gmail.com>
2024-02-21 20:00:00 +0200

    edit setup-file e018

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup
 
 setup(
     name='virtual-assistant-v007',
-    version='0.0.3',
+    version='0.0.5',
     description='Personal assistant helps you organize your notes and address book.',
-    url='https://github.com/prominb/personal-assistant2',
+    url='https://github.com/basilegupov/personal-notebook',
     author='Olds coders',
     author_email='flyingcircus@example.com',
     license='MIT',
@@ -12,10 +12,6 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"],
-    packages=find_namespace_packages(),
-    data_files=[('virtual_assistant_v007', ['virtual_assistant_v007/contacts.json', 'virtual_assistant_v007/notes.json'])],
-    include_package_data=True,
-    package_data={"virtual_assistant_v007": ["*.json", "*.txt"]},
     install_requires=[
         'prompt-toolkit>=3.0'],
     entry_points={'console_scripts': ['run-assistant = virtual_assistant_v007.main:run_bot']},
